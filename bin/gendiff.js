5bb4d209c542bdd8d4fc1d2644efac6419c8bce6
Timur <polshamangood@gmail.com>
2023-11-20 12:35:44 -0800

    fix

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse();
+

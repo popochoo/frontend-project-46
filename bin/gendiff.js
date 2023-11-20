5562b83660e39858e5265f7d34a16365cf50e6e3
Timur <polshamangood@gmail.com>
2023-11-20 13:02:04 -0800

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,10 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<firstpath1> <firstpath2>');
 
 program.parse();
 
